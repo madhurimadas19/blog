61d74fe7a0f5fb44c03b16a8cd2cc31373daa7d4
circar23 <circar23@circar23-desktop.(none)>
2014-03-25 14:14:00 +0530

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  # for new post
+  def new
+  end
+  
+  # For create post
+  def create
+    @post = Post.create(post_params)
+    redirect_to new_post_path
+  end
+  
+  def index
+    
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c1165cce7a80b21c11b6cb6598d0f461b7572f484ed3226a26b7e00d30f25f07709827cc45b8cfb88ff012965e54db592893ef895aa182bd8325a21c33cf8447'
